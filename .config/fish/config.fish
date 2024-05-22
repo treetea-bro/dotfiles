df60ac7ec4cbb82e65bcfb3e3a3a1a6b8f613bb1
seungmin <sorpwjdafh@naver.com>
2024-05-22 23:09:51 +0900

    fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,62 @@
+if status is-interactive
+    and not set -q TMUX
+    if tmux has-session 2>/dev/null; or test $status -eq 1
+        tmux a
+    else
+        tmux
+    end
+    # Commands to run in interactive sessions can go here
+end
+
+# if status is-interactive
+#     and not set -q TMUX
+#     exec tmux a
+#     # Commands to run in interactive sessions can go here
+# end
+~/.local/bin/mise activate fish | source
+
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+switch (uname)
+    case Darwin
+        source (dirname (status --current-filename))/config-osx.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
+    case '*'
+        source (dirname (status --current-filename))/config-windows.fish
+end
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+    source $LOCAL_CONFIG
+end
