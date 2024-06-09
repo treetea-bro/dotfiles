1a1a693f4c914dc87c5e60195ddcb89a572d2dc5
peco <sorpwjdafh@naver.com>
2024-06-09 13:18:00 +0900

    refactor(coding): make mason separate file

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,46 +1,5 @@
 return {
   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "selene",
-        "luacheck",
-        "shellcheck",
-        "shfmt",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "prisma-language-server",
-        "css-lsp",
-        "debugpy",
-        "js-debug-adapter",
-        "node-debug2-adapter",
-      },
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "kristijanhusak/vim-dadbod-completion",
-    },
-    -- override the options table that is used in the `require("cmp").setup()` call
-    opts = function(_, opts)
-      -- opts parameter is the default options table
-      -- the function is lazy loaded so cmp is able to be required
-      local cmp = require("cmp")
-      -- modify the sources part of the options table
-      opts.sources = cmp.config.sources({
-        { name = "nvim_lsp", priority = 1000 },
-        { name = "luasnip", priority = 750 },
-        { name = "buffer", priority = 500 },
-        { name = "path", priority = 250 },
-        { name = "vim-dadbod-completion", priority = 700 }, -- add new source
-      })
-      -- return the new table to be used
-      return opts
-    end,
-  },
-  {
     "smjonas/inc-rename.nvim",
     cmd = "IncRename",
     config = true,
