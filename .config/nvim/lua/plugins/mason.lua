10d9a0092a42d04d55873f898fd5246bd28d0df0
peco <sorpwjdafh@naver.com>
2024-06-11 09:26:30 +0900

    fix(rust): add rust-analyzer and commented extras.rust

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,21 +1,22 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "selene",
-        "luacheck",
-        "shellcheck",
-        "shfmt",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "prisma-language-server",
-        "css-lsp",
-        "debugpy",
-        "js-debug-adapter",
-        "node-debug2-adapter",
-      },
-    },
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"selene",
+				"luacheck",
+				"shellcheck",
+				"shfmt",
+				"tailwindcss-language-server",
+				"typescript-language-server",
+				"prisma-language-server",
+				"css-lsp",
+				"debugpy",
+				"js-debug-adapter",
+				"node-debug2-adapter",
+				"rust-analyzer",
+			},
+		},
+	},
 }
