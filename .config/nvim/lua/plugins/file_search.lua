60b1e640988b77c4562d917c1be9ce4d7bd51089
peco <sorpwjdafh@naver.com>
2024-06-22 16:49:26 +0900

    fix(tree-sitter): hide .sh files

--- .config/nvim/lua/plugins/file_search.lua
+++ .config/nvim/lua/plugins/file_search.lua
@@ -1,22 +1,25 @@
 return {
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          visible = false,
-          show_hidden_count = true,
-          hide_by_name = {
-            ".git",
-            -- ".gitignore",
-            ".DS_Store",
-            -- 'thumbs.db',
-          },
-          hide_dotfiles = false,
-          never_show = {},
-        },
-      },
-    },
-  },
+	{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			filesystem = {
+				filtered_items = {
+					visible = false,
+					show_hidden_count = true,
+					hide_by_pattern = {
+						"*.sh",
+					},
+					hide_by_name = {
+						".git",
+						-- ".gitignore",
+						".DS_Store",
+						-- 'thumbs.db',
+					},
+					hide_dotfiles = false,
+					never_show = {},
+				},
+			},
+		},
+	},
 }
