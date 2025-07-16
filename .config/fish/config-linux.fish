46117c9348c462997a00b6d6948dba360c19281c
seungmin <seungmin@maymust.com>
2025-07-16 09:44:16 +0900

    eza -T --git-ignore

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -1,4 +1,5 @@
 if type -q eza
     alias ll "eza -l -g --icons"
     alias lla "ll -a"
+    alias lt "eza -T --git-ignore"
 end
--- .config/fish/config-osx.fish
+++ .config/fish/config-osx.fish
@@ -1,6 +1,7 @@
 if type -q eza
     alias ll "eza -l -g --icons"
     alias lla "ll -a"
+    alias lt "eza -T --git-ignore"
 end
 
 # Inkdrop
