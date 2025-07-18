655bc93cc06ef662a697f74f2b42f46fd25373db
seungmin <seungmin@maymust.com>
2025-07-18 14:59:48 +0900

    pyright & ruff recognize nvim workspace

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,12 +3,16 @@ return {
 		"neovim/nvim-lspconfig",
 		opts = function(_, opts)
 			local configs = require("lspconfig.configs")
+			local util = require("lspconfig.util")
+
+			local python_root_pattern = util.root_pattern("pyproject.toml")
+
 			if not configs.pyrefly then
 				configs.pyrefly = {
 					default_config = {
 						cmd = { "pyrefly", "lsp" },
 						filetypes = { "python" },
-						root_dir = require("lspconfig.util").root_pattern("pyproject.toml"),
+						root_dir = python_root_pattern,
 						settings = {},
 						single_file_support = true,
 					},
@@ -17,6 +21,32 @@ return {
 
 			opts.servers = opts.servers or {}
 			opts.servers.pyrefly = {}
+
+			opts.servers.pyright = {
+				root_dir = python_root_pattern,
+				settings = {
+					python = {
+						analysis = {
+							autoSearchPaths = true,
+							useLibraryCodeForTypes = true,
+							diagnosticMode = "workspace",
+							autoImportCompletions = true,
+						},
+						-- 가상환경 사용하는 경우 venv 경로 설정
+						venvPath = vim.fn.getcwd() .. "/.venv",
+						pythonPath = vim.fn.getcwd() .. "/.venv/bin/python",
+					},
+				},
+			}
+
+			opts.servers.ruff_lsp = {
+				root_dir = python_root_pattern,
+				init_options = {
+					settings = {
+						args = {},
+					},
+				},
+			}
 
 			opts.servers.vtsls = {
 				settings = {
