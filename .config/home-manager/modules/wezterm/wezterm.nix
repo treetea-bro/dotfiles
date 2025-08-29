d6c02966a860a574f560ade58b1fd3bf5cbebab1
peco <sorpwjdafh@naver.com>
2025-08-29 15:12:37 +0900

    fix(wezterm) leader as SUPER && deactivate SUPER + l default action (close wezterm)

--- .config/home-manager/modules/wezterm/wezterm.nix
+++ .config/home-manager/modules/wezterm/wezterm.nix
@@ -15,9 +15,14 @@
 
       config.default_gui_startup_args = { "connect", "workspace" }
 
-      config.leader = { key = 'l', mods = 'ALT', timeout_milliseconds = 2000 }
+      config.leader = { key = 'l', mods = 'SUPER', timeout_milliseconds = 2000 }
 
       config.keys = {
+        {
+          key = "l",
+          mods = "SUPER",
+          action = wezterm.action.Nop,
+        },
         {
           key = "d",
           mods = "LEADER",
