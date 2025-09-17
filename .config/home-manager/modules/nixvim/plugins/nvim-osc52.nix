4e0111266965059938b96941d1508d8214b34a51
peco <sorpwjdafh@naver.com>
2025-09-17 18:01:33 +0900

    feat(nixvim) osc52 leader y

--- .config/home-manager/modules/nixvim/plugins/nvim-osc52.nix
+++ .config/home-manager/modules/nixvim/plugins/nvim-osc52.nix
@@ -5,30 +5,31 @@
     plugins = {
       nvim-osc52 = {
         enable = true;
-        keymaps.enable = false;
+        keymaps = {
+          enable = true;
+        };
       };
     };
 
-    extraConfigLua = ''
-      local osc52 = require("osc52")
-      osc52.setup { max_length = 0, silent = true, trim = false }
-
-      local function copy(lines, regtype)
-        local text = table.concat(lines, "\n")
-
-        osc52.copy(text)
-      end
-
-      local function paste()
-        return vim.fn.getreg('"', 1, true), vim.fn.getregtype('"')
-      end
-
-      vim.g.clipboard = {
-        name = "osc52",
-        copy = { ["+"] = copy, ["*"] = copy },
-        paste = { ["+"] = paste, ["*"] = paste },
-      }
-
-    '';
+    # extraConfigLua = ''
+    #   local osc52 = require("osc52")
+    #   osc52.setup { max_length = 0, silent = true, trim = false }
+    #
+    #   local function copy(lines, regtype)
+    #     local text = table.concat(lines, "\n")
+    #
+    #     osc52.copy(text)
+    #   end
+    #
+    #   local function paste()
+    #     return vim.fn.getreg('"', 1, true), vim.fn.getregtype('"')
+    #   end
+    #
+    #   vim.g.clipboard = {
+    #     name = "osc52",
+    #     copy = { ["+"] = copy, ["*"] = copy },
+    #     paste = { ["+"] = paste, ["*"] = paste },
+    #   }
+    # '';
   };
 }
