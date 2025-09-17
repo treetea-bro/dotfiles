69929af6e74b034c142894aa2a9d80153e99ce70
peco <sorpwjdafh@naver.com>
2025-09-17 15:28:20 +0900

    fix(osc52): 뒤에 유니코드 붙는 에러 제거

--- .config/home-manager/modules/nixvim/plugins/nvim-osc52.nix
+++ .config/home-manager/modules/nixvim/plugins/nvim-osc52.nix
@@ -13,17 +13,22 @@
       local osc52 = require("osc52")
       osc52.setup { max_length = 0, silent = true, trim = false }
 
-      local function copy(lines, _)
-        osc52.copy(table.concat(lines, "\n"))
+      local function copy(lines, regtype)
+        local text = table.concat(lines, "\n")
+
+        osc52.copy(text)
       end
+
       local function paste()
-        return { vim.fn.getreg('"') }, vim.fn.getregtype('"')
+        return vim.fn.getreg('"', 1, true), vim.fn.getregtype('"')
       end
+
       vim.g.clipboard = {
         name = "osc52",
         copy = { ["+"] = copy, ["*"] = copy },
         paste = { ["+"] = paste, ["*"] = paste },
       }
+
     '';
   };
 }
