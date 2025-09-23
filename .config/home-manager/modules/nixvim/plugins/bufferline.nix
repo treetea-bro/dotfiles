dee43aa2c405f3a1426eeba7d3bc4e5a4b229f88
peco <sorpwjdafh@naver.com>
2025-09-23 15:15:33 +0900

    feat(nixvim) bufferline move && cycle!!

--- .config/home-manager/modules/nixvim/plugins/bufferline.nix
+++ .config/home-manager/modules/nixvim/plugins/bufferline.nix
@@ -21,6 +21,30 @@
         action = "<Cmd>BufferLineGroupClose ungrouped<CR>";
         options.desc = "Delete non-pinned buffers";
       }
+      {
+        mode = "n";
+        key = "<A-h>";
+        action = "<Cmd>BufferLineMovePrev<CR>";
+        options.desc = "Move buffer left";
+      }
+      {
+        mode = "n";
+        key = "<A-l>";
+        action = "<Cmd>BufferLineMoveNext<CR>";
+        options.desc = "Move buffer right";
+      }
+      {
+        mode = "n";
+        key = "H";
+        action = "<Cmd>BufferLineCyclePrev<CR>";
+        options.desc = "Cycle buffer left (UI order)";
+      }
+      {
+        mode = "n";
+        key = "L";
+        action = "<Cmd>BufferLineCycleNext<CR>";
+        options.desc = "Cycle buffer right (UI order)";
+      }
     ];
   };
 }
