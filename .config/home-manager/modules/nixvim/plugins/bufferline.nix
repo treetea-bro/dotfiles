4cb3e12114fc0527fb208bf0e8ad58fc8f49e82b
peco <sorpwjdafh@naver.com>
2025-09-23 14:54:36 +0900

    feat(nixvim) bufferline add pin

--- .config/home-manager/modules/nixvim/plugins/bufferline.nix
+++ .config/home-manager/modules/nixvim/plugins/bufferline.nix
@@ -7,5 +7,20 @@
         enable = true;
       };
     };
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>bp";
+        action = "<Cmd>BufferLineTogglePin<CR>";
+        options.desc = "Toggle pin";
+      }
+      {
+        mode = "n";
+        key = "<leader>bP";
+        action = "<Cmd>BufferLineGroupClose ungrouped<CR>";
+        options.desc = "Delete non-pinned buffers";
+      }
+    ];
   };
 }
