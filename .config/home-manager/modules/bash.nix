f069e9eae667e05827bddb336a9e849d18315a26
peco <sorpwjdafh@naver.com>
2026-02-04 10:04:20 +0900

    feat(bash) added fish commend in .bashrc

--- .config/home-manager/modules/bash.nix
+++ .config/home-manager/modules/bash.nix
@@ -3,12 +3,18 @@
 {
   programs.bash = {
     enable = true;
+    enableCompletion = true;
+
     bashrcExtra = ''
       set -o vi
       bind -m vi-command 'Control-l: clear-screen'
       bind -m vi-insert 'Control-l: clear-screen'
+
+      if [[ $- == *i* ]] && [[ -z "$FISH_VERSION" ]]; then
+        exec ${pkgs.fish}/bin/fish
+      fi
     '';
-    enableCompletion = true;
+
     sessionVariables = {
       VISUAL = "nvim";
       EDITOR = "nvim";
