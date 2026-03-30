44a282cf61f1769c4f8bf4b6053df1f7d382f4f5
peco <sorpwjdafh@naver.com>
2026-03-30 15:13:18 +0900

    fix(bufferline): pin 된것들 우선순위 높히고, 컨트롤 1,2,3 등으로 버퍼 이동 추가

--- .config/home-manager/modules/nixvim/plugins/bufferline.nix
+++ .config/home-manager/modules/nixvim/plugins/bufferline.nix
@@ -2,9 +2,27 @@
 
 {
   programs.nixvim = {
-    plugins = {
-      bufferline = {
-        enable = true;
+    plugins.bufferline = {
+      enable = true;
+      settings = {
+        options = {
+          # 핀(Pin)된 버퍼를 항상 목록의 앞쪽(왼쪽)으로 정렬
+          persist_buffer_sort = true;
+          # 핀 된 버퍼가 우선순위를 갖도록 그룹화 설정
+          groups = {
+            items = [
+              {
+                name = "Pinned";
+                priority = 2;
+                matcher = ''
+                  function(buf)
+                    return vim.api.nvim_buf_get_var(buf.id, 'pinned')
+                  end
+                '';
+              }
+            ];
+          };
+        };
       };
     };
 
@@ -45,6 +63,17 @@
         action = "<Cmd>BufferLineCycleNext<CR>";
         options.desc = "Cycle buffer right (UI order)";
       }
+
+      { mode = "n"; key = "<C-1>"; action = "<Cmd>BufferLineGoToBuffer 1<CR>"; options.desc = "Go to buffer 1"; }
+      { mode = "n"; key = "<C-2>"; action = "<Cmd>BufferLineGoToBuffer 2<CR>"; options.desc = "Go to buffer 2"; }
+      { mode = "n"; key = "<C-3>"; action = "<Cmd>BufferLineGoToBuffer 3<CR>"; options.desc = "Go to buffer 3"; }
+      { mode = "n"; key = "<C-4>"; action = "<Cmd>BufferLineGoToBuffer 4<CR>"; options.desc = "Go to buffer 4"; }
+      { mode = "n"; key = "<C-5>"; action = "<Cmd>BufferLineGoToBuffer 5<CR>"; options.desc = "Go to buffer 5"; }
+      { mode = "n"; key = "<C-6>"; action = "<Cmd>BufferLineGoToBuffer 6<CR>"; options.desc = "Go to buffer 6"; }
+      { mode = "n"; key = "<C-7>"; action = "<Cmd>BufferLineGoToBuffer 7<CR>"; options.desc = "Go to buffer 7"; }
+      { mode = "n"; key = "<C-8>"; action = "<Cmd>BufferLineGoToBuffer 8<CR>"; options.desc = "Go to buffer 8"; }
+      # 9번은 마지막 버퍼로 이동
+      { mode = "n"; key = "<C-9>"; action = "<Cmd>BufferLineGoToBuffer -1<CR>"; options.desc = "Go to last buffer"; }
     ];
   };
 }
