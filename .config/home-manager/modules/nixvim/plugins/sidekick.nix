0f91ff825b9a88c71de477cb3b43a59f8151b8d2
seungmin <seungmin@maymust.com>
2026-03-30 23:48:27 +0900

    fix(copilot-lua): inline suggestion 다시 추가

--- .config/home-manager/modules/nixvim/plugins/sidekick.nix
+++ .config/home-manager/modules/nixvim/plugins/sidekick.nix
@@ -17,9 +17,27 @@
         enable = true;
         settings = {
           suggestion = {
-            enabled = false;
+            enabled = true;
+            auto_trigger = true;
+            debounce = 75;
+            keymap = {
+              accept = "<Tab>";
+            };
           };
           panel = { enabled = false; };
+
+          filetypes = {
+            "*" = true;
+
+            sh.__raw = ''
+              function()
+                if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+                  return false
+                end
+                return true
+              end
+            '';
+          };
         };
       };
 
