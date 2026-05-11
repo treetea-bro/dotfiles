e6c1cc3dc59bf2fb927eb3bbf9b3a7bfe385bba1
seungmin <seungmin@maymust.com>
2026-05-11 16:14:25 +0900

    feat(ghq): fish function added for ghq clone && get now clone after cd that folder

--- .config/home-manager/modules/fish.nix
+++ .config/home-manager/modules/fish.nix
@@ -26,5 +26,23 @@
         tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Vertical --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Dotted --powerline_right_prompt_frame=Yes --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Many icons' --transient=No
       end
     '';
+
+    functions = {
+      ghq = ''
+        switch "$argv[1]"
+          case clone get
+            command ghq $argv
+            or return $status
+            set -l url $argv[-1]
+            set -l name (string replace -r '\.git$' "" -- $url | string replace -r '^.*[:/]([^:/]+/[^/]+)$' '$1')
+            set -l path (command ghq list -p -e $name | head -n1)
+            if test -n "$path"
+              cd $path
+            end
+          case '*'
+            command ghq $argv
+        end
+      '';
+    };
   };
 }
